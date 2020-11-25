8f2e73215ce0b8263867117353f91a9088e0f576
Justin Sobieski <justin@sobieski.codes>
2020-11-25 07:28:58 -0500

    Fix for missions

--- Gainz4Dayz.js
+++ Gainz4Dayz.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         Gainz4Dayz
 // @namespace    http://sobieski.codes
-// @version      0.3
-// @description  Don't make the mistake of attacking #Ohadik
+// @version      0.4
+// @description  Don't make the mistake of attacking
 // @author       ProbsJustin#0001
 // @match        *.torn.com/loader.php*
-// @updateURL    https://raw.githubusercontent.com/SobieskiCodes/scripts/main/Gainz4Dayz.js
+// @updateURL    https://gist.githubusercontent.com/SobieskiCodes/f32bc09d38c2757606dc7db027363b2a/raw/278ca1baaadb72d7a24f0dd801546825e5cbb862/gainzallday.js
 // @connect      api.torn.com
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
-if(!document.location.href.includes("attackLog")){
+if(document.location.href.includes("sid=attack&user2ID=")){
     location.replace("https://www.torn.com/gym.php");
 }
